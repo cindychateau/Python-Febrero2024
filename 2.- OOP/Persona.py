c819fe00a8d4b8716d570e09cfdc4ce989ce8f9f
Cynthia Castillo <ca.castillo@gmail.com>
2024-02-20 17:20:53 -0800

    Atributos y Metodos de Clase. Metodos estaticos

--- 2.- OOP/Persona.py
+++ 2.- OOP/Persona.py
@@ -1,5 +1,8 @@
 class Persona:
 
+    escuela = "Coding Dojo"
+    lista_personas = []
+
     '''
     self = juana
     nombre = "Juana"
@@ -8,13 +11,15 @@ class Persona:
     hobbies = ["Bailar", "Programar"]
     '''
     #A través del método init INICIALIZAMOS/CREAMOS una nueva instancia
-    def __init__(self, nombre, apellido, email, hobbies, edad, lineas_codigo):
+    def __init__(self, nombre, apellido, email, hobbies, edad):
         self.nombre = nombre #juana.nombre = "Juana"
         self.apellido = apellido #juana.apellido = "De Arco"
         self.email = email #juana.email = "juana@codingdojo.com"
         self.hobbies = hobbies #juana.hobbies = ["Bailar", "Programar"]
         self.edad = edad
         self.lineas_codigo = 0 #lineas de código que ha desarrollado
+        Persona.lista_personas.append(self)
+
     
     #self -> la instancia que está invocando al método
     def saludar(self):
@@ -27,4 +32,18 @@ class Persona:
     def codificar(self, cantidad_lineas):
         self.lineas_codigo += cantidad_lineas
 
-
+    #Método que pertenece a mi clase y que llama o hace referencia a atributo de clase NO A INSTANCIAS
+    @classmethod
+    def imprimir_personas(cls):
+        #persona = elena
+        #persona = juana
+        for p in cls.lista_personas:
+            print(p.nombre, p.apellido)
+    
+    #Método estático: Algo que NO pertenece a la instancia, que NO pertenece a la clase, Ayuda/Organizar nuestro código
+    @staticmethod
+    def mayor_edad(edad):
+        if edad > 17:
+            return True
+        else:
+            return False
--- 2.- OOP/main.py
+++ 2.- OOP/main.py
@@ -21,3 +21,16 @@ juana.codificar(10) #lineas_codigo = 10
 elena.codificar(50) #lineas_codigo = 50
 elena.codificar(100) #lineas_codigo = 150
 print(elena.lineas_codigo)
+
+print(Persona.lista_personas)
+Persona.imprimir_personas()
+
+if Persona.mayor_edad(elena.edad):
+    print("Si es mayor")
+else:
+    print("No es mayor")
+
+if Persona.mayor_edad(30):
+    print("Si es mayor")
+else:
+    print("No es mayor")
