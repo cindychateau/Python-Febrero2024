d99df5842d5a1aa6334e7a12e93bff16a5b32f2a
Cynthia Castillo <ca.castillo@gmail.com>
2024-04-09 18:24:33 -0600

    Intro a Algoritmos

---
+++ 5.- Algoritmos/Intro/algoritmos.py
@@ -0,0 +1,23 @@
+'''
+FACTORIAL
+Recibir un número y regresar el factorial de este
+1! = 1*1 = 1
+2! = 1*2 = 2
+3! = 1*2*3 = 6
+4! = 1*2*3*4 = 24
+'''
+
+'''
+FIBONACCI
+Recibimos un número entero e imprimimos la serie de fibonacci en esa posición
+Serie Fibonacci: Comenzando con 0 y 1, vamos sumando los dos últimos números
+0 1 1 2 3 5 8 13 21 34 55 89
+5 -> 0 1 1 2 3 5
+'''
+
+'''
+PALINDROMO
+Recibimos una cadena y nos regresa True o False si la palabra es palíndroma
+Ejemplo: oso, salas, seres, radar
+ana -> True
+'''
