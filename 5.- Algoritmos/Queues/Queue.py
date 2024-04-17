baeaee779b3406e9097872b87f87da03ae42c9d4
Cynthia Castillo <ca.castillo@gmail.com>
2024-04-16 20:20:45 -0600

    enqueue, dequeue, impresion

--- 5.- Algoritmos/Queues/Queue.py
+++ 5.- Algoritmos/Queues/Queue.py
@@ -7,10 +7,27 @@ class Queue:
         self.tail = None
     
     def enqueue(self, nuevo_nodo):
-        pass
+        #1.- Revisar si mi head estaba vacío. Entonces nuevo_nodo es el primero de la lista
+        if self.head == None:
+            self.head = nuevo_nodo
+        else:
+            #2.- Si existe algo en mi queue, al último nodo le pongo como siguiente el nuevo_nodo
+            ultimo_nodo = self.tail
+            ultimo_nodo.next = nuevo_nodo
+        #3.- Establezco el nuevo_nodo como el final de mi queue
+        self.tail = nuevo_nodo
     
     def dequeue(self):
-        pass
+        if self.head == self.tail:
+            self.tail = None
+        
+        nodo_actual = self.head
+        self.head = nodo_actual.next
+        nodo_actual.next = None
+        return nodo_actual
     
     def impresion(self):
-        pass
+        aux = self.head
+        while aux != None:
+            print(aux.valor)
+            aux = aux.next
