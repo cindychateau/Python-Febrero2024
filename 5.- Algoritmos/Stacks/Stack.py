92d2eca6f0231511b31b80ca56f147a03f4455d1
Cynthia Castillo <ca.castillo@gmail.com>
2024-04-18 18:45:33 -0600

    Stack base

---
+++ 5.- Algoritmos/Stacks/Stack.py
@@ -0,0 +1,15 @@
+from Nodo import Nodo
+
+class Stack:
+
+    def __init__(self):
+        self.top = None
+    
+    def push(self, nuevo_nodo):
+        pass
+    
+    def pop(self):
+        pass
+    
+    def impresion(self):
+        pass
