28c74e997ed9f35eb51694007f4aa8089b3a351c
Cynthia Castillo <ca.castillo@gmail.com>
2024-04-30 18:44:30 -0600

    Comenzamos con ListaEncadenada

---
+++ 5.- Algoritmos/ListaEncadenadas/ListaEncadenada.py
@@ -0,0 +1,15 @@
+from Nodo import Nodo
+
+class ListaEncadenada:
+
+    def __init__(self):
+        self.head = None #Lo único que necesitamos es saber cuál es el primer nodo de la lista. Al inicializar la lista es vacía, el head es None
+    
+    def insertaNodo(self, nuevoNodo):
+        pass
+    
+    def imprimeLista(self):
+        pass
+    
+    def eliminaNodo(self, valor):
+        pass
---
+++ 5.- Algoritmos/ListaEncadenadas/Nodo.py
@@ -0,0 +1,5 @@
+class Nodo:
+
+    def __init__(self, valor):
+        self.valor = valor
+        self.next = None #El next se refiere al siguiente en la lista. Como mi nodo No pertenece aún a una lsita, entonces el siguiente en NONE
